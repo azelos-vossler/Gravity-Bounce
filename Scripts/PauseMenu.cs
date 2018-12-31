a842c5d194db154acdbae76ac14c20bf11720589
azelos-vossler <35978773+azelos-vossler@users.noreply.github.com>
2018-12-31 12:15:07 -0600

    Weeks 4-6(no reduction yet)

---
+++ Scripts/PauseMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public Transform canvas;
+
+   void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                canvas.gameObject.SetActive(false);
+                Time.timeScale = 1f;
+            }
+        }
+    }
+}
