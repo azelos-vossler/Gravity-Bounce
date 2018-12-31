0ca8dcc3f08650cdcfad37fc9de1883316a25deb
azelos-vossler <35978773+azelos-vossler@users.noreply.github.com>
2018-12-31 13:00:38 -0600

    Week 6 (paddle Reduction)

---
+++ PaddleReduction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleReduction : MonoBehaviour
+{
+   
+    
+	void OnCollisionEnter2D (Collision2D other)
+    {
+      if (other.gameObject.tag == "Ball")
+      {
+        transform.localScale -= new Vector3(.05f, 0, 0);
+       
+      }
+         
+    }
+	
+}
